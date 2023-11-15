6472eee2984d8f44ec66b6472ea71fcbe69115b2
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-15 15:25:06 -0600

    inheritance 1

--- Tutorial10/Source.cpp
+++ Tutorial10/Source.cpp
@@ -15,6 +15,13 @@ public:
 	void report();
 };
 
+class Dog :public Animal {
+public:
+	Dog();
+	Dog(string Name, int Age, int Limbs);
+
+	void Speak();
+};
 
 int main() {
 
@@ -24,11 +31,13 @@ int main() {
 	Animal animal_2("Dog", 6, 4);
 	//animal_2.report();
 
+	Dog dog("Halen", 6, 4);
+	dog.Speak();
 	system("pause");
 }
 
 Animal::Animal() {
-	cout << "animal is born \n";
+	cout << "Animal is born \n";
 	name = "DEAFUALT";
 	age = 0;
 	limbs = 4;
@@ -43,4 +52,17 @@ void Animal::report() {
 	cout << "Name: " << name << endl;
 	cout << "Age: " << age << endl;
 	cout << "Limbs: " << limbs << endl;
+	cout << endl;
+}
+
+Dog::Dog() {
+	cout << "Dog is born" << endl;
+}
+
+Dog::Dog(string Name, int Age, int Limbs){
+	Animal(Name, Age, Limbs);
+}
+
+void Dog::Speak() {
+	cout << "Boof!" << endl;
 }
