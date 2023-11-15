85f7e37f5236c60f1f24f52d7b0ec1f907b1b52c
NRowley <nicholas.ryan.rowley@gmail.com>
2023-11-15 15:14:02 -0600

    overload constructors

--- Tutorial10/Source.cpp
+++ Tutorial10/Source.cpp
@@ -2,15 +2,45 @@
 #include <string>
 using namespace std;
 
+class Animal
+{
+public:
+	Animal();
+	Animal(string Name, int Age, int Limbs);
 
+	string name;
+	int age;
+	int limbs;
+
+	void report();
+};
 
 
 int main() {
 
+	Animal animal;
+	animal.report();
 
-
-
-
+	Animal animal_2("Dog", 6, 4);
+	//animal_2.report();
 
 	system("pause");
+}
+
+Animal::Animal() {
+	cout << "animal is born \n";
+	name = "DEAFUALT";
+	age = 0;
+	limbs = 4;
+}
+
+Animal::Animal(string Name, int Age, int Limbs): name(Name), age(Age), limbs(Limbs) {
+	cout << Name << " is born \n";
+	report();
+}
+
+void Animal::report() {
+	cout << "Name: " << name << endl;
+	cout << "Age: " << age << endl;
+	cout << "Limbs: " << limbs << endl;
 }
